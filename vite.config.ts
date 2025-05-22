8106b682fedd0ee9d6099e92815a5bcd261fbc2c
user <aqusay06@gmail.com>
2025-05-22 18:44:58 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'binary-bridges-ui',
+  base: '/binary-bridges-ui/',
   plugins: [react()],
 })
