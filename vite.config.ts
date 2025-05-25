cb63652db0c5456cabe95f1164ef963a9b2e9cd9
user <aqusay06@gmail.com>
2025-05-25 10:48:36 +0300

    solve routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig, UserConfig} from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -7,7 +7,7 @@ export default defineConfig({
   preview: {
     port: parseInt(process.env.PORT || '4173'),
     host: '0.0.0.0',
-    allowedHosts: 'all'
+    allowedHosts: ['binary-bridges-ui.onrender.com', 'localhost']
   },
   build: {
     rollupOptions: {
