0bc771a76a4883a9381343582d9f9fbe7bb585ac
user <aqusay06@gmail.com>
2025-05-25 10:43:08 +0300

    solve routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {defineConfig, UserConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -8,5 +8,12 @@ export default defineConfig({
     port: parseInt(process.env.PORT || '4173'),
     host: '0.0.0.0',
     allowedHosts: 'all'
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
-})
+} as UserConfig)
